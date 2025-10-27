0edb8115d93e5396250c31f93e2d7d599bd994d6
강세아 <gangsea817@gmail.com>
2025-10-27 13:32:25 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C99/백준/Bronze/10810. 공 넣기/공 넣기.c
+++ C99/백준/Bronze/10810. 공 넣기/공 넣기.c
@@ -2,15 +2,15 @@
 
 int main() {
     int n, m;
-    int arr[101] = {0};  // 바구니 배열 (1~N 사용)
+    int arr[101] = {0};
 
     scanf("%d %d", &n, &m);
 
     for (int t = 0; t < m; t++) {
         int i, j, k;
-        scanf("%d %d %d", &i, &j, &k);  // 구간과 공 번호 입력받기
+        scanf("%d %d %d", &i, &j, &k);
         for (int x = i; x <= j; x++) {
-            arr[x] = k;  // 해당 구간에 공 번호 채우기
+            arr[x] = k;
         }
     }
 
